f2ab7bd88f99daab33d851bb0c57cbc75ffe534d
fakel_ast <lvov-styopa@ya.ru>
2021-10-04 09:01:23 +0300

    Changed database and some settings

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # app
     # 'api'
+    'apps.users'
 
 ]
 
@@ -85,11 +86,16 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pde',
+        'USER': 'pde',
+        'PASSWORD': '6NR%Zzr9s',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -133,8 +139,18 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = ''
 
+# Media
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# custom
+
+URLS_FOR_VUE = ['tasks', '1', '2']
