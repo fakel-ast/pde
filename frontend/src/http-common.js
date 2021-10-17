ff2a5df0966685a1d4f01052124768224899707c
fakel_ast <lvov-styopa@ya.ru>
2021-10-17 18:59:46 +0300

    changed base url

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8000/api/',
+  baseURL: process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:8000/api/',
   headers: {
     post: {
       'Content-Type': 'application/json'
