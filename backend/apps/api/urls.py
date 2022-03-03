b8cd821112d8368d49512fa9cad420bb7a251b4c
Stepan <lvov-styopa@ya.ru>
2022-03-03 16:14:15 +0300

    Added api for get categories

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -1,3 +1,4 @@
 from backend.apps.api import views
 from backend.apps.api import api
 
+api.add_url_rule('/categories/', view_func=views.CategoriesViews.as_view('category_view'))
--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -5,6 +5,35 @@ from flask import json, request, abort, jsonify, current_app, render_template, R
 from peewee import fn, JOIN, SQL, CharField, NodeList
 from werkzeug.exceptions import BadRequest
 
+from backend.database.models import TaskCategory
 from backend.functions import recursive_parse
-from backend.base import MyMethodView, CONFIG, ModalMixin
+from backend.base import MyMethodView, CONFIG
 
+
+class CategoriesViews(MyMethodView):
+
+    def get(self, *args, **kwargs):
+        """Get categories"""
+
+        try:
+            categories = TaskCategory.select(
+                TaskCategory.id,
+                TaskCategory.title,
+                TaskCategory.created,
+                fn.CONCAT(
+                    CONFIG.PATH_TO_TASK_CATEGORIES_IMAGES, '/',
+                    TaskCategory.id, '/',
+                    TaskCategory.image
+                )
+            ).where(
+                TaskCategory.active
+            ).dicts()
+
+            categories = recursive_parse(list(categories))
+
+            return {'errors': False, 'categories': categories}
+
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            current_app.logger.error('Get categories: ' + format(e) + ' in ' + format(exc_tb.tb_lineno))
+            return {'errors': True, 'message': 'Code error'}, 500
