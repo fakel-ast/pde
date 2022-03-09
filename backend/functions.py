6a0ebeb6e86f44e8cec5b94ed5be3327c79944b5
fakel_ast <lvov-styopa@ya.ru>
2022-03-09 22:54:13 +0300

    +

--- backend/functions.py
+++ backend/functions.py
@@ -79,7 +79,7 @@ def recursive_parse(dict_for_parse):
 def get_current_user(*args, **kwargs):
     """Model select for get current user"""
     from backend.base import CONFIG
-    from database.models import User
+    from backend.database.models import User
     user = User.select(
         User.id,
         User.username,
